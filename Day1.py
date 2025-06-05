c4d9d06d72988f38fcc649cf344ae6be28634b05
up240763 <up240763@alumnos.upa.edu.mx>
2025-06-05 13:07:04 -0600

    probando commit

---
+++ Day1.py
@@ -0,0 +1,24 @@
+# EXCERSISE: LEVEL 1
+
+print(3 + 4) #addition
+print(4 - 3) #subtration
+print(4 * 3) #multiplication
+print(4 % 3) #modulus
+print(4 / 3) #division
+print(4 ** 3) #exponential
+print(3 // 4) #floor division
+
+# EXERCISE : LEVEL 2
+
+print(type('José Alberto'))  # String
+print(type({'Quiroz Macias'})) 
+print(type({"Mexíco"}))    
+print(type({'I am enjoying 30 days of python'}))
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j)) #numero complejo 
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(("José Alberto"))
+print(("Quiroz Macias"))
+print(("Mexíco"))
