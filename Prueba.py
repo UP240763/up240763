a3fb8f325aa7f76b18d959470481bcc75e343461
up240763 <up240763@alumnos.upa.edu.mx>
2025-07-16 20:48:45 -0600

    funciones

--- Prueba.py
+++ Prueba.py
@@ -1,25 +1,9 @@
 
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
-language = 'Python'
-formated_string = 'I am {} {}. I teach {}'.format(first_name, last_name, language)
-print(formated_string)
-a = 4
-b = 3
-
-print('{} + {} = {}'.format(a, b, a + b))
-print('{} - {} = {}'.format(a, b, a - b))
-print('{} * {} = {}'.format(a, b, a * b))
-print('{} / {} = {:.2f}'.format(a, b, a / b)) # limits it to two digits after decimal
-print('{} % {} = {}'.format(a, b, a % b))
-print('{} // {} = {}'.format(a, b, a // b))
-print('{} ** {} = {}'.format(a, b, a ** b))
-
-
-
-# Strings  and numbers
-radius = 10
-pi = 3.14
-area = pi * radius ** 2
-formated_string = 'The area of a circle with a radius {} is {:.2f}.'.format(radius, area) # 2 digits after decimal
-print(formated_string)
+for i in range(11):
+    print("------------")
+    print(f"Tabla del {i}")
+    for j in range (11):
+        result = i * j 
+        print(f"{i} x {j} = {result}")
+    print("------------")
+    print()
